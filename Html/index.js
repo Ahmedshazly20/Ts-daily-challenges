e3f169892c7fea306c68bd70152a816f22da5145
Ahmed shazly <67927284+Ahmedshazly20@users.noreply.github.com>
2026-01-05 23:27:43 +0200

    call stack js

--- Html/index.js
+++ Html/index.js
@@ -1,11 +1,14 @@
-
-
-const arr = [1, -4, 7, 12];
-
-function positiveSum(arr) {
-
-    newArr = arr.filter(num => num > 0);
-    const sumsq = newArr.reduce((acc, curr) => acc + curr, 0);
-    return sumsq;
-
+function greet() {
+    console.log("Hello!");
 }
+
+function sayName(name) {
+    console.log("My name is " + name);
+    greet();  // استدعاء دالة greet
+}
+
+function introduce() {
+    sayName("Alice");  // استدعاء دالة sayName
+}
+
+introduce();  // استدعاء الدالة الرئيسية
